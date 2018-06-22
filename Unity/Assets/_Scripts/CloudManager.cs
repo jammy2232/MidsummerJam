b0ca99d3b67e46d6a9a43c68e2c1489866e5f7bc
James McAdam <j.g.mcadam@gmail.com>
2018-06-22 10:09:08 +0100

    Added some basic boiler plate for creating particles systems and integrating them into the game.

---
+++ Unity/Assets/_Scripts/CloudManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour 
+{
+
+	// Holds all the effects selected to be placed in the scene
+	public List<Effect> CloudEffects;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		// if an input is fired check what effects should be activated
+		if (Input.anyKeyDown) 
+		{
+
+			foreach(var effect in CloudEffects)
+			{
+				if(Input.GetKeyDown(effect.KeyToActivate))
+				{
+					effect.ParticleSystem.Play ();
+				}
+
+			}
+		}
+
+		// if an input is released check what effects should be deactivated
+		foreach (var effect in CloudEffects) 
+		{
+			if(effect.ParticleSystem.isPlaying)
+			{
+				if (!Input.GetKeyDown (effect.KeyToActivate)) 
+				{
+					effect.ParticleSystem.Stop ();
+				}
+			}
+
+		}
+
+	}
+}
---
+++ Unity/Assets/_Scripts/Effect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CloudEffect", menuName = "CloudEffect")]
+public class Effect : ScriptableObject {
+
+	// This is a container to generate different types of effects 
+	public ParticleSystem ParticleSystem;
+
+	// The key with which to action this system\
+	[Tooltip("This should correspond to one of the Makey Makey Key inputs")]
+	public KeyCode KeyToActivate;
+
+}
