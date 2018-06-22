f17f0877afb4d0b232d89057ced2c347b39b39fb
James McAdam <j.g.mcadam@gmail.com>
2018-06-22 11:58:06 +0100

    Added examples of placing effects in the scene

--- Unity/Assets/_Scripts/CloudManager.cs
+++ Unity/Assets/_Scripts/CloudManager.cs
@@ -5,12 +5,34 @@ using UnityEngine;
 public class CloudManager : MonoBehaviour 
 {
 
-	// Holds all the effects selected to be placed in the scene
+	// Holds all the effects selected tobe placed in the scene
 	public List<Effect> CloudEffects;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+
+		int counter = 0;
+
+		// Check that all the Effects are valid
+		foreach(var effect in CloudEffects)
+		{
+			if (!effect.ParticleSystem.GetComponent<ParticleSystem> ()) 
+			{
+				Debug.Log (effect.name + " has NO Particle System");
+			} 
+			else 
+			{
+				effect.effectHolder = Instantiate(effect.ParticleSystem);
+				effect.effectHolder.transform.position = GetPosition (counter, CloudEffects.Count);
+				effect.effectHolder.AddComponent<AudioSource> ();
+
+			}
+
+			counter++;
+				
+		}
+
 	}
 	
 	// Update is called once per frame
@@ -25,24 +47,53 @@ public class CloudManager : MonoBehaviour
 			{
 				if(Input.GetKeyDown(effect.KeyToActivate))
 				{
-					effect.ParticleSystem.Play ();
+					effect.effectHolder.GetComponent<ParticleSystem>().Play();
 				}
-
 			}
 		}
 
 		// if an input is released check what effects should be deactivated
+
 		foreach (var effect in CloudEffects) 
 		{
-			if(effect.ParticleSystem.isPlaying)
+			if(effect.effectHolder.GetComponent<ParticleSystem>().isPlaying)
 			{
-				if (!Input.GetKeyDown (effect.KeyToActivate)) 
+				if (!Input.GetKey (effect.KeyToActivate)) 
 				{
-					effect.ParticleSystem.Stop ();
+					effect.effectHolder.GetComponent<ParticleSystem>().Stop ();
 				}
 			}
 
 		}
 
 	}
+
+
+	// Function to return the position of a particle system based the number of systems and it's place in the list
+	Vector3 GetPosition(int position, int totalNumberofSystems)
+	{
+
+		// Get the main Camerea W value
+		float ScreenHeightUnits = Camera.main.orthographicSize * 2.0f;
+		float ScreenWidthUnits = ScreenHeightUnits * Screen.width / Screen.height;
+
+		// Catch for one system
+		if (totalNumberofSystems == 1)
+			return new Vector3 (0.0f, -5.0f, 0.0f);
+
+		// the y and z positions are fixed as the visuals are 3d 
+		float denomimator = totalNumberofSystems + 1;
+
+		if (denomimator == 0.0f)
+			return new Vector3 (0.0f, 0.0f, 0.0f);
+
+		// Calculate the positions 
+		float xpositions = -(0.5f * ScreenWidthUnits) + ((float)position + 1.0f) * ScreenWidthUnits / denomimator;
+
+		// Return the positions relative to the number of systems
+		return new Vector3(xpositions, -5.0f, 0.0f);
+	
+	}
+
+
 }
--- Unity/Assets/_Scripts/Effect.cs
+++ Unity/Assets/_Scripts/Effect.cs
@@ -6,10 +6,26 @@ using UnityEngine;
 public class Effect : ScriptableObject {
 
 	// This is a container to generate different types of effects 
-	public ParticleSystem ParticleSystem;
+	public GameObject ParticleSystem;
 
 	// The key with which to action this system\
 	[Tooltip("This should correspond to one of the Makey Makey Key inputs")]
 	public KeyCode KeyToActivate;
+
+	// References to the music item this effect will play
+	[Header("Audio Effects")]
+	public AudioClip musicClip;
+	[Range(-1.0f, 0.0f)]
+	public float pitch;
+	[Range(-1.0f, 0.0f)]
+	public float tempo;
+	[Range(0.0f, 0.0f)]
+	public float fadeIn;
+	[Range(0.0f, 0.0f)]
+	public float fadeOut;
+
+	[HideInInspector]
+	public GameObject effectHolder;
+
 
 }
