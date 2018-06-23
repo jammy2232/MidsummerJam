0cfb777e9025db3c31aa84be686fc4833bdda521
Duncan <blank>
2018-06-23 11:24:29 +0100

    Comment and refactor Effect

--- Unity/Assets/_Scripts/Effect.cs
+++ Unity/Assets/_Scripts/Effect.cs
@@ -14,54 +14,55 @@ public class Effect : ScriptableObject {
 	[Header("Audio Effects")]
 	public AudioMixerGroup musicMixer;
 	public AudioMixerSnapshot RequiredSnapShot;
+	// Looping music clip that fades in and out on button press
 	public AudioClip musicClip;
 	public AudioMixerGroup soundMixer;
+	// Non looping sound clip that plays once on button press
 	public AudioClip soundsClip;
+	// For music clips
 	public float FadeInRate;
 	public float FadeOutRate;
 
-	// Instanciate the effect setup to work
+	// Spawn location is where particles will generate at.
 	public CloudStream SpawnEffect(Vector3 SpawnLocation)
 	{
-		// Create the Object
+		// Create the particle system that generates our cloud particles
 		GameObject newEffect = Instantiate(ParticleSystem);
 
-		// Check the effect has a Particle System otherwise return Null and log the error
-		if (!newEffect.GetComponent<ParticleSystem> ()) 
+		// Check the effect has a Particle System
+		if (!newEffect.GetComponent<ParticleSystem>()) 
 		{
+			// This should never happen but its best to be safe.
 			Debug.Log (newEffect.name + " has NO Particle System");
 			return null;
 		} 
 
-		// Position it 
+		// Position where the particle generator will spawn particles from 
 		newEffect.transform.position = SpawnLocation;
 
-		// Add and assign the Audio Parameters 
-
-		// Add the components for Music and Sound Music ref 0 and Sound ref 1
+		/// Audio Parameters
+		// 2 components for Music (looping with fadeing) and Sound (non-looping no fade)
 		newEffect.AddComponent<AudioSource>();
 		newEffect.AddComponent<AudioSource>();
 
-		// get references to edit these sources as per the object paramaters 
+		// get references to edit these sources 
 		AudioSource[] sources = newEffect.GetComponents<AudioSource>();
 
-		// Setup the music 
+		// Setup the music
 		sources[0].clip = musicClip;
+		sources[0].loop = true;
 		sources[0].outputAudioMixerGroup = musicMixer;
 
-		// Setup the audio 
+		// Setup the sound
 		sources[1].clip = soundsClip;
-		sources [1].loop = false;
+		sources[1].loop = false;
 		sources[1].outputAudioMixerGroup = soundMixer;
 
-		// Add a CloudSteamComponent and set it up
+		// Add a CloudStreamComponent and set it up
 		newEffect.AddComponent<CloudStream>();
 		newEffect.GetComponent<CloudStream>().snapshot = RequiredSnapShot;
 		newEffect.GetComponent<CloudStream>().Init();
 
-		// Return the gameObject Reference 
 		return newEffect.GetComponent<CloudStream>();
-
 	}
-		
 }
