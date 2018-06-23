690a0d83cd1f4c201627e14c27ba25ad50464357
Duncan <blank>
2018-06-23 11:40:37 +0100

    Comment and refactor wind

--- Unity/Assets/_Scripts/GlobalWind.cs
+++ Unity/Assets/_Scripts/GlobalWind.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Note this has not been designed in a normal Unity friendly way
 public class GlobalWind 
 {
-
+	// Anything heigher than the windHeight is affected by wind
 	public float windHeight = 10.5f;
-	public float windDirection = 10.5f;
+	// Magnitude and direction of the wind
+	public float windDirection = 1.0f;
+	// Requires access to the CloudStream to make wind blow the particles
 	public CloudStream[] objects;
 
 	// Constructor
@@ -18,28 +21,30 @@ public class GlobalWind
 	// Update is called once per frame
 	public void Update () 
 	{
-		
-		// Blow some wind at a predefined height
+		// Access all of the particles that are currently in play for each stream
+		// and give them some wind if above the predefined height
 		foreach (CloudStream obj in objects)
-		{
-			
+		{		
 			ParticleSystem.Particle[] streamsParticles = new ParticleSystem.Particle[obj.particles.main.maxParticles];
 
 			// GetParticles is allocation free because we reuse the streamsParticles buffer between updates
 			int numParticlesAlive = obj.particles.GetParticles(streamsParticles);
 
 			// Change only the particles that are alive
-			for (int i = 0; i < numParticlesAlive; i++)
+			for(var i = 0; i < streamsParticles.Length; ++i)
 			{
 				if (streamsParticles[i].position.z > windHeight)
 				{
+					// We add/remove more x velocity over the lifetime of the particle
+					// this makes it a bit more realistic compared to sticking to the
+					// one x velocity. Remember also there can be x, y, z velocity on the particles
+					// from the particle generator
 					streamsParticles[i].velocity += new Vector3(Mathf.Clamp(windDirection, -1.0f, 1.0f), 0.0f, 0.0f) * 0.5f;
 				}
 			}
 
 			// Apply the particle changes to the particle system
 			obj.particles.SetParticles(streamsParticles, numParticlesAlive);
-
 		}
 	}
 }
